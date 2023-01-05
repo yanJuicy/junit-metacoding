bc491ed5d6252a21f691612e5479755eb68b71c6
yanJuicy <hanjub3@gmail.com>
2023-01-05 17:07:39 +0900

    서비스레이어 책등록

--- src/main/java/com/metacoding/junitproject/service/BookService.java
+++ src/main/java/com/metacoding/junitproject/service/BookService.java
@@ -1,8 +1,25 @@
 package com.metacoding.junitproject.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.metacoding.junitproject.domain.Book;
+import com.metacoding.junitproject.domain.BookRepository;
+import com.metacoding.junitproject.web.dto.BookRespDto;
+import com.metacoding.junitproject.web.dto.BookSaveReqDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Service
 public class BookService {
+
+    private final BookRepository bookRepository;
+
+    @Transactional(rollbackFor = RuntimeException.class)
+    public BookRespDto 책등록하기(BookSaveReqDto dto) {
+        Book bookPS = bookRepository.save(dto.toEntity());
+        return new BookRespDto().toDto(bookPS);
+    }
 
 }
--- src/main/java/com/metacoding/junitproject/web/dto/BookRespDto.java
+++ src/main/java/com/metacoding/junitproject/web/dto/BookRespDto.java
@@ -1,5 +1,31 @@
 package com.metacoding.junitproject.web.dto;
 
+import com.metacoding.junitproject.domain.Book;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
 public class BookRespDto {
-    
+
+    private Long id;
+    private String title;
+    private String author;
+
+    @Builder
+    public BookRespDto(Long id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+    public BookRespDto toDto(Book bookPS) {
+        return BookRespDto.builder()
+                .id(bookPS.getId())
+                .title(bookPS.getTitle())
+                .author(bookPS.getAuthor())
+                .build();
+    }
 }
