ccebbbf07bedd607c487c8d4489d8b4e8a4b0408
yanJuicy <hanjub3@gmail.com>
2023-01-06 12:05:30 +0900

    컨트롤러 레이어 본코드 - 책 저장하

--- src/main/java/com/metacoding/junitproject/web/BookApiController.java
+++ src/main/java/com/metacoding/junitproject/web/BookApiController.java
@@ -1,8 +1,57 @@
 package com.metacoding.junitproject.web;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.metacoding.junitproject.service.BookService;
+import com.metacoding.junitproject.web.dto.reqquest.BookSaveReqDto;
+import com.metacoding.junitproject.web.dto.response.BookRespDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @RestController
 public class BookApiController {
+
+    private final BookService bookService;
+
+    @PostMapping("/api/v1/book")
+    public ResponseEntity<?> saveBook(@RequestBody BookSaveReqDto bookSaveReqDto, BindingResult bindingResult) {
+        BookRespDto bookRespDto = bookService.책등록하기(bookSaveReqDto);
+        return new ResponseEntity<>(bookRespDto, HttpStatus.CREATED); // 201 = insert
+    }
+
+    public ResponseEntity<?> getBookList() {
+        return null;
+    }
+
+    // 3. 책한건보기
+    @GetMapping("/api/v1/book/{id}")
+    public ResponseEntity<?> getBookOne(@PathVariable Long id) {
+        return null;
+
+    }
+
+    // 4. 책삭제하기
+    @DeleteMapping("/api/v1/book/{id}")
+    public ResponseEntity<?> deleteBook(@PathVariable Long id) {
+        return null;
+
+    }
+
+    // 5. 책수정하기
+    @PutMapping("/api/v1/book/{id}")
+    public ResponseEntity<?> updateBook(@PathVariable Long id, @RequestBody BookSaveReqDto bookSaveReqDto,
+            BindingResult bindingResult) {
+        return null;
+    }
 
 }
